47324325cf8330a391fb860ba310805a5da478d6
User <User@LAB3-3>
2023-11-06 11:24:26 +0200

    777

--- FoundItServer/Program.cs
+++ FoundItServer/Program.cs
@@ -1,11 +1,36 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using FountItBL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+#region Json Hendeling
+builder.Services.AddControllers().
+    AddJsonOptions(o => o.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve);
+#endregion
 
-builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+
+#region 2- Session Support
+//Add Session support
+builder.Services.AddDistributedMemoryCache(); builder.Services.AddSession(options =>
+{
+options.IdleTimeout = TimeSpan.FromMinutes(180);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+
+});
+#endregion
+
 
 var app = builder.Build();
 
@@ -20,6 +45,23 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+
+
+#region Use FILES And Session. //use files 
+app.UseStaticFiles(); 
+app.UseRouting();
+#region Use Session
+app.UseSession();
+#endregion 
+#endregion
+
 app.MapControllers();
 
 app.Run();
+
+#region DBCONTEXT
+//load connection string
+string connection = builder.Configuration.GetConnectionString("FoundItDBS"); 
+//Add DBContext
+builder.Services.AddDbContext<FoundItDbContext>(options=>options.UseSqlServer(connection));
+#endregion
