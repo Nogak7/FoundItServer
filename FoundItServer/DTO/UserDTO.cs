3894f81e2441343000946120e689bf8598927ecc
User <User@LAB3-3>
2024-03-21 12:23:59 +0200

    11

---
+++ FoundItServer/DTO/UserDTO.cs
@@ -0,0 +1,24 @@
+﻿using FountItBL.Models;
+
+namespace FoundItServer.DTO
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+
+        public string Email { get; set; } 
+
+        public string FirstName { get; set; } 
+
+        public string LastName { get; set; } 
+
+        public string Pasword { get; set; } 
+        public string UserName { get; set; } 
+
+        public virtual ICollection<Community> Communities { get; set; } = new List<Community>();
+
+        public virtual ICollection<CommunityMember> CommunityMembers { get; set; } = new List<CommunityMember>();
+
+        public virtual ICollection<Post> Posts { get; set; } = new List<Post>();
+    }
+}
